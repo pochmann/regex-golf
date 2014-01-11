fb61fd08bbf64e87c0e70143714f251d2cd0bc0d
Stefan Pochmann <stefan.pochmann@gmail.com>
2014-01-11 19:38:12 +0100

    Create improvements.py

---
+++ improvements.py
@@ -0,0 +1,18 @@
+"""Improvements to Peter's supporting functions."""
+
+# Components of the form a.*b, a.+b and a.?b help get better results:
+
+def regex_components(winners, losers):
+    ...
+    pairs = {p for w in winners for p in paaairs(w) if not matches(p, losers)}
+    return wholes | parts | pairs
+
+def paaairs(w):
+    return {w[i]+'.'+q+b for i in range(len(w)) for b in w[i+1:] for q in '*+?'}
+
+# Pre-compiling the regex components speeds things up a little:
+
+def matches(regex, strings):
+    "Return a set of all the strings that are matched by regex."
+    r = re.compile(regex)
+    return {s for s in strings if r.search(s)}
